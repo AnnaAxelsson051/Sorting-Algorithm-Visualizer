940480e29b220af0acce898f820edfe551cdc1b9
Anna Axelsson <a.axelsson51@gmail.com>
2023-02-04 01:33:49 +0100

    Adjust bar colors

--- index.js
+++ index.js
@@ -49,17 +49,18 @@ async function bubbleSort(array){
             if (array[j] > array[j + 1]){
                 for(let k = 0; k<bars.length; k++){
                     if (k !== j && k !== j +1){
-                        bars[k].style.backgroundColor = "aqua";
+                       bars[k].style.backgroundColor = "blue";
                     }
                 }
                 let temp = array[j];
                 array[j] = array[j +1];
                 array[j + 1] = temp;
                 bars[j].style.height = array[j] * heightFactor + "px";
-                bars[j].style.backgroundColor = "lightgreen";
+                bars[j].style.backgroundColor = "pink";
+                
                 //bars[j].innerText = array[j];
                 bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
-                bars[j + 1].style.backgroundColor = "lightgreen";
+                bars[j + 1].style.backgroundColor = "pink";
                 //bars[j + 1]. innerText = array[j + 1];
                 await sleep(1);
             }
